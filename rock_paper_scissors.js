66aec99c9f7e6c42428c9f740f0bed748eeacef0
rastarecords3 <rastarecords3@gmail.com>
2025-10-10 22:04:12 +0100

    all cases for inputs are lower now, and user input is case-insensitive

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,16 +4,16 @@ function getComputerChoice() {
     const choice = Math.floor(Math.random() *3) +1;
     // console.log(choice);
     switch (choice) {
-        case 1: return 'Rock'
-        case 2: return 'Paper'
-        case 3: return 'Scissors'
-        default: return 'Error'
+        case 1: return 'rock'
+        case 2: return 'paper'
+        case 3: return 'scissors'
+        default: return 'error'
     }
 }
 
 // function to get human's choice of rock/paper/scissors
 function getHumanChoice() {
-    const choice = prompt('Enter your choice of Rock/Paper/Scissors','')
+    const choice = prompt('Enter your choice of rock/paper/scissors','')
     return choice
 }
 
@@ -24,14 +24,14 @@ let computerScore = 0
 // create a function to play a round
 function playRound() {
     // get the choices
-    const humanChoice = getHumanChoice()
-    const computerChoice = getComputerChoice()
+    const humanChoice = getHumanChoice().toLowerCase();
+    const computerChoice = getComputerChoice();
     // return {humanChoice, computerChoice};
     if (humanChoice === computerChoice) {console.log(`You chose ${humanChoice}, the computer chose ${computerChoice} it's a draw.`)}
     else if (
-        (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
-        (humanChoice === 'Paper' && computerChoice === 'Rock') ||
-        (humanChoice === 'Scissors' && computerChoice === 'Rock')
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'rock')
             )
         {console.log(`You chose ${humanChoice}, the computer chose ${computerChoice} you win this round!`)}
     else
