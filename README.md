3f958775d4a603bb503b1a3882dc4efc7ccf2a19
rastarecords3 <rastarecords3@gmail.com>
2025-10-10 21:45:11 +0100

    logic for R/P/S rounds complete and tested

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 javascript projects creted as coding exercises for the odin project course
 
 10/10/25 - Project 1. Rock Paper Scissors started
-Step 1. Write a function that randomly returns “rock”, “paper” or “scissors”.
 
--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,40 @@
-console.log("Hello World")
+// function to get computer's choice of rock/paper/scissors
+function getComputerChoice() {
+    // get a random integer from 1 to 3
+    const choice = Math.floor(Math.random() *3) +1;
+    // console.log(choice);
+    switch (choice) {
+        case 1: return 'Rock'
+        case 2: return 'Paper'
+        case 3: return 'Scissors'
+        default: return 'Error'
+    }
+}
+
+// function to get human's choice of rock/paper/scissors
+function getHumanChoice() {
+    const choice = prompt('Enter your choice of Rock/Paper/Scissors','')
+    return choice
+}
+
+// declare score variables
+let humanScore = 0
+let computerScore = 0
+
+// create a function to play a round
+function playRound() {
+    // get the choices
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+    // return {humanChoice, computerChoice};
+    if (humanChoice === computerChoice) {console.log(`You chose ${humanChoice}, the computer chose ${computerChoice} it's a draw.`)}
+    else if (
+        (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+        (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+        (humanChoice === 'Scissors' && computerChoice === 'Rock')
+            )
+        {console.log(`You chose ${humanChoice}, the computer chose ${computerChoice} you win this round!`)}
+    else
+        {console.log (`You chose ${humanChoice}, the computer chose ${computerChoice} you loose this round.`)}
+    return {humanChoice, computerChoice};
+}
