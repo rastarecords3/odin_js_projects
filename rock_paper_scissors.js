05a3bb2c6e64dbd25dab87cc1ebaf0aae2102d54
rastarecords3 <rastarecords3@gmail.com>
2025-10-18 00:43:01 +0100

    refactored the win logic with the added a win condition array, and converted the console log messages in the playround function to a message appended by the DOM

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,11 +1,30 @@
+// use the DOM to insert the dynamic message
+const body = document.body
+const displayMessage = document.createElement("div")
+displayMessage.textContent = "Play a game with me"
+body.appendChild(displayMessage)
+
+// set up button click event listeners to retrieve the user choice and pass it to the playround function
 const buttons = document.querySelectorAll("button")
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const humanChoice = button.id
-        alert(`You chose ${humanChoice}`)
+        console.log(`You chose ${humanChoice}`)
         playRound(humanChoice)
     })
 })
+
+// array of winning combinations
+const winMap = {
+    'rock': 'scissors',
+    'paper': 'rock',
+    'scissors': 'paper',
+};
+
+// declare score variables
+let humanScore = 0
+let computerScore = 0
+let round = 1
 
 // function to get computer's choice of rock/paper/scissors
 function getComputerChoice() {
@@ -20,35 +39,24 @@ function getComputerChoice() {
     }
 }
 
-// function to get human's choice of rock/paper/scissors
-function getHumanChoice() {
-    const choice = prompt('Enter your choice of rock/paper/scissors','')
-    return choice
-}
-
-// declare score variables
-let humanScore = 0
-let computerScore = 0
-let round = 1
-
 // function to play a round with humanChoice as a parameter 
 function playRound(humanChoice) {
     const computerChoice = getComputerChoice();
-    // return {humanChoice, computerChoice};
-    if (humanChoice === computerChoice) {console.log(`You chose ${humanChoice}, the computer chose ${computerChoice} it's a draw.`);
-        console.log(`Human score: ${humanScore} - Computer score: ${computerScore}`)}
-    else if (
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')
-            )
-        {humanScore++;
-        console.log(`You chose ${humanChoice}, the computer chose ${computerChoice} you win this round!`);
-        console.log(`Human score: ${humanScore} - Computer score: ${computerScore}`)}
+    let message = ''
+    
+    if (humanChoice === computerChoice) {
+        message = `You chose ${humanChoice}, the computer chose ${computerChoice} it's a draw.`
+    }
+    else if (winMap[humanChoice] === computerChoice) {
+        humanScore++;
+        message = `You chose ${humanChoice}, the computer chose ${computerChoice} you win this round!`
+    }
     else
         {computerScore++;
-        console.log (`You chose ${humanChoice}, the computer chose ${computerChoice} you loose this round.`)
-        console.log(`Human score: ${humanScore} - Computer score: ${computerScore}`)}
+        message = `You chose ${humanChoice}, the computer chose ${computerChoice} you lose this round.`
+    }
+
+    displayMessage.textContent = `${message} (Human: ${humanScore} - Computer: ${computerScore})`;
     return {humanChoice, computerChoice};
 }
 
