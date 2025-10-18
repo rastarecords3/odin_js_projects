395ec64b2f23b8b87330eb48b2d8765afd0ea38c
rastarecords3 <rastarecords3@gmail.com>
2025-10-18 20:21:21 +0100

    added reset/clear grid button, and change grid size button and functionality for both

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,20 +1,43 @@
 const container = document.querySelector("#container")
+const resetBtn = document.querySelector("#reset")
+const resizeBtn = document.querySelector("#resize")
 
 let squaresPerSide =16
-let numberOfSquares = squaresPerSide * squaresPerSide
 
-for (let i = 0; i < numberOfSquares; i++) {
-    const div = document.createElement("div")
-    div.classList.add("grid-item")
-    div.style.flex = `0 0 calc(100% / ${squaresPerSide})`
-    div.style.aspectRatio = "1 / 1"
-    div.style.border = "1px solid";
-    div.style.boxSizing = "border-box";
+function makeGrid(size) {
+    container.innerHTML = ""
+    const numberOfSquares = size * size
 
-    // hover behaviour
-    div.addEventListener("mouseenter", () => {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const div = document.createElement("div")
+        div.classList.add("grid-item")
+        div.style.flex = `0 0 calc(100% / ${squaresPerSide})`
+        div.style.aspectRatio = "1 / 1"
+        div.style.border = "1px solid";
+        div.style.boxSizing = "border-box";
+        // hover behaviour
+        div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = `hsl(${Math.random() * 360}, 80%, 50%)`;
-    })
-
-    container.appendChild(div)
+        })
+        container.appendChild(div)
+    }
 }
+
+makeGrid(squaresPerSide)
+
+resetBtn.addEventListener("click", () => {
+    document.querySelectorAll(".grid-item").forEach(div => {
+        div.style.backgroundColor = "";
+    })
+})
+
+resizeBtn.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Enter a new grid side length (1-50):"), 10)
+    if (newSize > 0 && newSize <=50) {
+        squaresPerSide = newSize
+        makeGrid(squaresPerSide)
+    } else {
+        alert("Please enter a number between 1 and 50")
+    }
+
+})
