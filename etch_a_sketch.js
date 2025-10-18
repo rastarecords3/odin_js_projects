d39567125a87488aa1f4710b6ee6f94dc4ac3c76
rastarecords3 <rastarecords3@gmail.com>
2025-10-18 19:41:24 +0100

    added the mouse enter event listener and div modifier to colour the squares with the mouse

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -8,5 +8,13 @@ for (let i = 0; i < numberOfSquares; i++) {
     div.classList.add("grid-item")
     div.style.flex = `0 0 calc(100% / ${squaresPerSide})`
     div.style.aspectRatio = "1 / 1"
+    div.style.border = "1px solid";
+    div.style.boxSizing = "border-box";
+
+    // hover behaviour
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = `hsl(${Math.random() * 360}, 80%, 50%)`;
+    })
+
     container.appendChild(div)
 }
